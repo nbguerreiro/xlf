689b27dcebbfe77412ae04928144b4469db0b5fd
nbguerreiro <79049764+nbguerreiro@users.noreply.github.com>
2026-09-05 17:57:20 +0100

    Extract preview cleanup functions

--- preview.c
+++ preview.c
@@ -1,5 +1,4 @@
 #include "preview.h"
-
 #include <stdlib.h>
 #include <glib-object.h>
 
@@ -15,7 +14,7 @@ extern char *preview_media_text;
 extern int preview_is_media;
 extern ScaledImageCache scaled_image_cache;
 
-void free_preview_image(void) {
+void free_preview_image() {
     if (preview_image) {
         g_object_unref(preview_image);
         preview_image = NULL;
@@ -23,31 +22,39 @@ void free_preview_image(void) {
     preview_is_image = 0;
 }
 
-void free_preview_html(void) {
-    free(preview_html_text);
-    preview_html_text = NULL;
+void free_preview_html() {
+    if (preview_html_text) {
+        free(preview_html_text);
+        preview_html_text = NULL;
+    }
     preview_is_html = 0;
 }
 
-void free_preview_pdf(void) {
-    free(preview_pdf_text);
-    preview_pdf_text = NULL;
+void free_preview_pdf() {
+    if (preview_pdf_text) {
+        free(preview_pdf_text);
+        preview_pdf_text = NULL;
+    }
     preview_is_pdf = 0;
 }
 
-void free_preview_text(void) {
-    free(preview_text_content);
-    preview_text_content = NULL;
+void free_preview_text() {
+    if (preview_text_content) {
+        free(preview_text_content);
+        preview_text_content = NULL;
+    }
     preview_is_text = 0;
 }
 
-void free_preview_media(void) {
-    free(preview_media_text);
-    preview_media_text = NULL;
+void free_preview_media() {
+    if (preview_media_text) {
+        free(preview_media_text);
+        preview_media_text = NULL;
+    }
     preview_is_media = 0;
 }
 
-void free_scaled_image_cache(void) {
+void free_scaled_image_cache() {
     if (scaled_image_cache.pixbuf) {
         g_object_unref(scaled_image_cache.pixbuf);
         scaled_image_cache.pixbuf = NULL;
@@ -55,3 +62,4 @@ void free_scaled_image_cache(void) {
     scaled_image_cache.width = 0;
     scaled_image_cache.height = 0;
 }
+
