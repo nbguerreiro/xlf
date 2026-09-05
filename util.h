b7e8926c7ab94ca15314370a9cdb60cc2576a0f0
nbguerreiro <79049764+nbguerreiro@users.noreply.github.com>
2026-09-05 18:15:22 +0100

    Expose utility tool helpers

--- util.h
+++ util.h
@@ -7,5 +7,7 @@ char *get_absolute_path(const char *path);
 char *get_display_path(const char *path);
 void format_file_info(const char *path, const char *name, int is_dir,
                       char *buf, int buf_size);
+int tool_is_available(const char *tool_name);
+void check_tool_availability(void);
 
 #endif
